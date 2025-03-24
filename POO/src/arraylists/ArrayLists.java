f8913be219652a832d43c74eeb73145d479c0334
Conrado Mendez Colomer <cmendezcolomer@gmail.com>
2025-03-24 18:21:03 -0300

    Lists. ArrayList

---
+++ POO/src/arraylists/ArrayLists.java
@@ -0,0 +1,41 @@
+package arraylists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayLists {
+
+	public static void main(String[] args) {
+		List<Persona> lista = new ArrayList<Persona>(); 
+		
+		
+		lista.add(new Persona(1, "Conrado", 30)); 
+		lista.add(new Persona(2, "Ben", 130)); 
+		lista.add(new Persona(3, "Ba", 350)); 
+		lista.add(new Persona(4, "MA", 63)); 
+		lista.add(new Persona(5, "Vane", 430)); 
+		
+		//Recorrer por indice con for clasico
+		System.out.println("---------For convencional:---------");	
+	for (int i=0;  i< lista.size(); i++ ) {
+		// usamos .size, no .lenght para listas 	
+		System.out.println("Prueba: " + lista.get(i).getNombre());
+		 
+	}
+	
+	// recorrido foreach 
+	System.out.println("---------Foreach loop:---------");
+	
+	for (Persona perso:lista) {
+		
+		int index = 0; 
+		if (perso.getNombre().equals("Conrado")) {
+			System.out.println("En la posicion: " + index + " se encuentra "   + perso.getNombre());
+		}
+		index ++; 
+	}
+
+
+	}
+
+}
